b71ade6193827bb3b893956afa6a4a51640a67c7
Danislav Konyarski <unstopable46@gmail.com>
2024-02-12 16:59:17 +0200

    new task

---
+++ src/DBAppsIntroduction/Task2.java
@@ -0,0 +1,43 @@
+package DBAppsIntroduction;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+
+        Properties properties = new Properties();
+        properties.setProperty("user", "root");
+        properties.setProperty("password", "1221");
+
+        Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/diablo", properties);
+
+        System.out.print("Enter ussername: ");
+        String username = scanner.nextLine();
+
+        PreparedStatement query = connection.prepareStatement(
+                "SELECT user_name, first_name, last_name, COUNT(ug.id) as games "+
+                "FROM users "+
+                "JOIN users_games as ug ON users.id = ug.user_id "+
+                "WHERE user_name = ? "+
+                "GROUP BY user_id;");
+        query.setString(1, username);
+
+        ResultSet resultSet = query.executeQuery();
+
+        if (resultSet.next()) {
+            System.out.printf("Users: %s%n%s %s has played %d games",
+                    resultSet.getString("user_name"),
+                    resultSet.getString("first_name"),
+                    resultSet.getString("last_name"),
+                    resultSet.getInt("games")
+                    );
+
+        } else {
+            System.out.println("No such user exists");
+        }
+    }
+}
